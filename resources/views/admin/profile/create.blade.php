279db0954eaeada78df42d827bba6fc39efbc32a
吉市 <rescue.masahiro.441@gmail.com>
2021-10-23 05:33:53 +0000

    PHP/Laraver11の課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -9,5 +9,16 @@
     </head>
     <body>
         <h1>プロフィール作成画面</h1>
+        @extends('layouts.profile')
+        @section('title','プロフィールの新規作成')
+        @section('content')
+          <div class="container">
+            <div class="row">
+                <div class="col-md-8 mx-auto">
+                    <h2>プロフィールの新規作成</h2>
+                </div>
+            </div>
+          </div>
+        @endsection
     </body>
 </html>

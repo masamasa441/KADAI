792b354b471569a520441baee5b03104107ba21f
吉市 <rescue.masahiro.441@gmail.com>
2021-11-04 05:09:55 +0000

    課題のデプロイ実行前の更新

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -13,7 +13,7 @@
       <div class="col-md-8">
           <from action="{{ action('Admin\NewsController@index') }}" method="get">
               <div class="form-group row">
-                  <label class="col-md-2">タイトル</label>
+                  <label class="col-md-2">ニュース検索</label>
                   <div class="col-md-8">
                       <input type="text" class="form-control" name="cond_title" value="{{ $cond_title }}">
                   </div>
